2025c6c463c23e4e1bc13c7f4eef7b33199f9c4b
fuzzyami <ami.blonder@kik.com>
2017-10-26 13:55:51 +0300

    added a new version of the backup script with better filters

---
+++ backup2.py
@@ -0,0 +1,72 @@
+'''
+This script is ran daily to take snapshots of instances' ebs devices.
+- the script will only target  
+'''
+import boto3
+import collections
+import datetime
+import sys
+
+ec = boto3.client('ec2')
+
+def backup(backupGroupName):
+    reservations = ec.describe_instances(
+        Filters=[ 
+            {'Name':'tag:backup', 'Values':['group1']},
+        ]
+    ).get(
+        'Reservations', []
+    )
+    print reservations
+
+    instances = [
+        i for r in reservations
+        for i in r['Instances']
+    ]
+
+    print "Found %d instances that need backing up" % len(instances)
+
+    to_tag = collections.defaultdict(list)
+
+    for instance in instances:
+        try:
+            retention_days = [
+                int(t.get('Value')) for t in instance['Tags']
+                if t['Key'] == 'Retention'][0]
+        except IndexError:
+            retention_days = 7
+
+        for dev in instance['BlockDeviceMappings']:
+            if dev.get('Ebs', None) is None:
+                continue
+            vol_id = dev['Ebs']['VolumeId']
+            print "Found EBS volume %s on instance %s" % (
+                vol_id, instance['InstanceId'])
+
+            snap = ec.create_snapshot(
+                VolumeId=vol_id,
+            )
+
+            to_tag[retention_days].append(snap['SnapshotId'])
+
+            print "Retaining snapshot %s of volume %s from instance %s for %d days" % (
+                snap['SnapshotId'],
+                vol_id,
+                instance['InstanceId'],
+                retention_days,
+            )
+
+
+    for retention_days in to_tag.keys():
+        delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
+        delete_fmt = delete_date.strftime('%Y-%m-%d')
+        print "Will delete %d snapshots on %s" % (len(to_tag[retention_days]), delete_fmt)
+        ec.create_tags(
+            Resources=to_tag[retention_days],
+            Tags=[
+                {'Key': 'DeleteOn', 'Value': delete_fmt},
+            ]
+        )
+
+if __name__=="__main__":
+    backup(sys.argv[1])
